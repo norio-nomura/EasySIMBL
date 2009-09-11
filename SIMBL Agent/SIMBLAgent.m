9c55c678647710b63f5608457d0bb7bbd96f21bf
Mike Solomon <mas63@cornell.edu>
2009-09-10 22:56:26 -0700

    fixed installer to check/repair permissions. expunged dead code from agent. added uninstaller.

--- SIMBL Agent/SIMBLAgent.m
+++ SIMBL Agent/SIMBLAgent.m
@@ -73,43 +73,6 @@ fail:
 		NSLog(@"launchctl returned %d", [task terminationStatus]);
 }
 
-#define SCRIPT_SRC_LEOPARD @"tell application \"%@\"\ninject SIMBL into Leopard\nend tell"
-#define SCRIPT_SRC_SNOW_LEOPARD @"tell application \"%@\"\ninject SIMBL into Snow Leopard\nend tell"
-
-- (void) _injectSIMBL:(NSNotification*)notification
-{
-	/* injectSIMBL in a generic way that works, but it isn't specific to the
-	inbound notification. the code below is attempting to be more specific by
-	referencing the PSN, but it doesn't actually work.
-	*/
-	NSLog(@"received %@", [notification userInfo]);
-	
-	unsigned majorOSVersion = 0;
-	unsigned minorOSVersion = 0;
-	unsigned buxfixOSVersion = 0;
-	[NSApp getSystemVersionMajor:&majorOSVersion minor:&minorOSVersion bugFix:&buxfixOSVersion];
-	
-	if (majorOSVersion != 10) {
-		NSLog(@"something fishy - OS X version %u", majorOSVersion);
-	}
-
-	NSString* script_src = nil;
-	if (minorOSVersion > 5) {
-		script_src = [NSString stringWithFormat:SCRIPT_SRC_SNOW_LEOPARD, 
-			[[notification userInfo] objectForKey:@"NSApplicationName"]];
-	}
-	else {
-		script_src = [NSString stringWithFormat:SCRIPT_SRC_LEOPARD, 
-			[[notification userInfo] objectForKey:@"NSApplicationName"]];
-	}
-  NSLog(@"sending %@", script_src);
-	NSAppleScript* script = [[NSAppleScript alloc] initWithSource:script_src];
-	NSDictionary* errorInfo = nil;
-	NSAppleEventDescriptor* reply = [script executeAndReturnError:&errorInfo];
-	if (reply == nil)
-		NSLog(@"errors %@", errorInfo);
-}
-
 - (void) injectSIMBL:(NSNotification*)notification
 {
 	NSLog(@"received %@", [notification userInfo]);
@@ -124,24 +87,23 @@ fail:
 		NSLog(@"something fishy - OS X version %u", majorOSVersion);
 		return;
 	}
+	
 	AEEventID eventID = minorOSVersion > 5 ? 'load' : 'leop';
 
-	
 	// Find the process to target
-	pid_t pid = [[[notification userInfo] objectForKey: @"NSApplicationProcessIdentifier"] intValue];
+	pid_t pid = [[[notification userInfo] objectForKey:@"NSApplicationProcessIdentifier"] intValue];
 	SBApplication *app = [SBApplication applicationWithProcessIdentifier: pid];
 	if (!app) {
 		NSLog(@"Can't find app with pid %d", pid);
 		return;
 	}
-	[app setSendMode: kAENoReply];
-	
+	[app setSendMode:kAENoReply];
 	
 	// Force AppleScript to initialize in the app, by getting the dictionary
-	[app sendEvent: kASAppleScriptSuite id: kGetAEUT parameters: 0];
+	[app sendEvent:kASAppleScriptSuite id:kGetAEUT parameters:0];
 	
 	// Inject!
-	[app sendEvent: 'SIMe' id: eventID parameters: 0];
+	[app sendEvent:'SIMe' id:eventID parameters:0];
 }
 
 @end
